b5f0cede576b30c4b33250fff9db0d57f60df196
rdkerr <rogerdkerr@gmail.com>
2019-01-25 18:27:02 -0700

    Added sql schema file

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,19 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+create table messages(id int primary key auto_increment not null, created_at timestamp, message varchar(200), room_id int, user_id int);
+
+create table users(id int primary key auto_increment not null, name varchar(25));
+
+create table rooms(id int primary key auto_increment not null, name varchar(25));
+
+create table friends(id int primary key auto_increment not null, friend_id int);
+
+alter table friends add foreign key (friend_id) references users(id);
+
+alter table messages add foreign key (user_id) references users(id);
+
+alter table messages add foreign key (room_id) references rooms(id);
 
 /* Create other tables and define schemas for them here! */
 
